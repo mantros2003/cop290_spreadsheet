d13fbbb067f357c79c20534ca3ad9ae257e3c69a
SaicharanPanugothu <166162433+Saicharan8688@users.noreply.github.com>
2025-02-19 00:49:23 +0530

    Update sheet_print.c

--- src/sheet_print.c
+++ src/sheet_print.c
@@ -1,11 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "database.h"
 #include "cell.h"
-#include "column.h"
+#include "cell.c"
+#include "database.h"
+#include "database.c"
 #include "hashset.h"
+#include "hashset.c"
 #include "nodes_ll.h"
+#include "nodes_ll.c"
+#include <math.h>
+#include <ctype.h>
+/* 
+ *  The following type is useful when we want segregate alphabetic string from 
+ *  numeric string of cell representation.
+ *  eg.AA23 Here alphabetic string is AA and nnumeric string is 23.
+ */
+typedef struct {
+    int numberPart;
+    char* stringPart;
+}int_string;
+/*
+ * The following functoin converts the given alphanumeric form 
+ * to it's numeric representation.
+ */
+int convertInt(char x[]){
+    int result=0;
+    int length=strlen(x);
+    int i=length-1;
+    int p=0;
+    while(i>=0){
+        char ch=x[i];
+        int req=ch-'A'+1;
+        result=result+(int)(pow(26,p)*req);
+        p++;
+        i--;
+    }
+    return result;
+}
+/*
+ *  The following function segregates the alphabetic string part and numeric string part
+ *  from the given cell representation.
+ */
+int_string* segregate(char* position){
+    int size=strlen(position);
+    int k=0;
+    char* stringPart=(char*) malloc(size*sizeof(char));
+    for(int i=0;i<size;i++){
+        if(!isalpha(position[i]) && !isdigit(position[i])){
+            printf("%s  ","You entered the wrong cell");
+            return NULL;
+        }
+        else if(isalpha(position[i])){
+            stringPart[k++]=position[i];
+        }
+        else if(isdigit(position[i])){
+            break;
+        }
+    }
+    int numberPart=0;
+    int power=size-k-1;
+    for(int i=k;i<size;i++){
+        int digit=position[i]-'0';
+        numberPart+=pow(10,power)*digit;
+        power--;
+    }
+    int_string* after_segregation=(int_string*) malloc(sizeof(int_string));
+    after_segregation->numberPart=numberPart;
+    after_segregation->stringPart=stringPart;
+    return after_segregation;
+}
+/*
+ *  The following function return a pointer to the cell that
+ *  corresponds with the given row and column numbers in the database.
+ */
+struct cell* get_cell_pointer(int row,int col,database* db){
+    struct column_segment *column_pointer=get_column(db,col);
+    return get_cell(row,&column_pointer);
+}
+void add_dep_range(struct cell *target, char* start,char* end, char oper,database *db){
+    target->oper=oper;
+    int_string* temp1=segregate(start);
+    int_string* temp2=segregate(end);
+    int start_row=temp1->numberPart;
+    int end_row=temp2->numberPart;
+    int start_col=convertInt(temp1->stringPart);
+    int end_col=convertInt(temp2->stringPart);
+    for(int i=start_row;i<=end_col;i++){
+        for(int j=start_col;j<=end_col;j++){
+            struct cell* from=get_cell_pointer(i,j,db);
+            add_dep(target,from,oper);
+        }
+    }
+    free(temp1);
+    free(temp2);
+
+}
 /*
  *Converts the colNumber to corresponding Alphabatic representation and returns.
  */
@@ -46,7 +136,7 @@ void print_sheet(database* db,int row,int col){
         printf("%-9s",convertAlpha(colnumber));
     }
     printf("\n");
-    for(int rowNumber=row;rowNumber<rowEnd;rowNumber++){
+    for(int rowNumber=row;rowNumber<(row+10);rowNumber++){
         printf("%-8d",rowNumber);
         for(int colNumber=col;colNumber<colEnd;colNumber++){
             if(has_error(db,colNumber,rowNumber)){
@@ -59,5 +149,16 @@ void print_sheet(database* db,int row,int col){
     }
 }
 int main(){
+    // char* x=(char*)malloc(2*sizeof(char));
+    // x[0]='x';
+    // int* y=(int*)malloc(sizeof(int));
+    // *y=2;
+    // print_int(*y);
+    // print_end();
+    // for(int i=0;i<40;i++){
+    //     printf("%s %d",x,*y);
+    //     get_next(&x,y);
+    //     printf("\n");
+    // 
     return 0;
 }
